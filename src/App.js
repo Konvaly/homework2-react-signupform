716bbf76c1fab8b90ec4e87f5ab1f8883901c499
Mariia <konvaly@gmail.com>
2021-09-01 10:49:58 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import SignUpForm from './components/SignUpform';
+
+function App () {
+  return <SignUpForm />;
+}
+
+export default App;
---
+++ src/components/SignUpform/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SignUpForm extends Component {
+  render () {
+    return (
+      <form>
+        <label>Login</label>
+      </form>
+    );
+  }
+}
+
+export default SignUpForm;
